393bfab8706f79567822bd78b895f6c63358625c
ninyo <ninyoaganon@gmail.com>
2016-05-16 13:53:56 -0700

    Updated Header.php and installed analytics

--- _library/link/php/header.php
+++ _library/link/php/header.php
@@ -1,21 +1,34 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Milbro Construction</title>
+	<?
+	$head_title = "Milbro Construction";
+	$head_description = "Your dream home is only a remodel away. A licensed owned general construction company owned by Eric Russell from Bremerton, WA. ";
+	?>
+	<title><? echo $head_title;?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-	<meta name="description" content="" />
-	<meta property="og:type" content="Company">
+	<meta name="description" content="<? echo $head_description;?>"/>
+	<meta property="og:title" content="<? echo $head_title;?>">
+	<meta property="og:description" content="<? echo $head_description;?>">
+	<meta property="og:type" content="Website">
 	<meta property="og:locale" content="en_US">
-	<meta property="og:title" content="Milbro Construction">
-	<meta property="og:description" content="Your dream home is only a remodel away.">
 	<meta property="og:image" content="http://i.imgur.com/K1tWzdH.png">
 	<meta property="og:url" content="http://milbroconstruction.com">
-	<?//<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">?>
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<!-- <link rel="icon" type="image/png" href="_library/img/asset/fav_icon.png"> -->
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-	<script src="http://cdnjs.cloudflare.com/ajax/libs/jQuery.mmenu/5.5.3/core/js/jquery.mmenu.min.all.js" type="text/javascript"></script>
+	<!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/jQuery.mmenu/5.5.3/core/js/jquery.mmenu.min.all.js" type="text/javascript"></script> -->
 	<link href='https://fonts.googleapis.com/css?family=Fira+Sans:400,500,300,700' rel='stylesheet' type='text/css'>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-77852700-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
 
 </head>
 <body style="background:#312b29;">
